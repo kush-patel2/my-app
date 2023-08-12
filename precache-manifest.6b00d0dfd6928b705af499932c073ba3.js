852b9d677c2e2887741550d4d2d3a3d3159f8499
kush-patel2 <kushp3220@gmail.com>
2023-08-12 13:15:07 +0530

    Updates

---
+++ precache-manifest.6b00d0dfd6928b705af499932c073ba3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5c3f0ef929415479c47",
+    "url": "/my-app/static/css/main.32a4f2ab.chunk.css"
+  },
+  {
+    "revision": "a5c3f0ef929415479c47",
+    "url": "/my-app/static/js/main.a5c3f0ef.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/my-app/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/my-app/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "8b16e4071cc3418912ee",
+    "url": "/my-app/static/js/runtime~main.8b16e407.js"
+  },
+  {
+    "revision": "47e10e0ef0182a5399456b8cb8b58ff2",
+    "url": "/my-app/index.html"
+  }
+];
